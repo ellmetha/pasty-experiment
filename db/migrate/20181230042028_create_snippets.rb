871e5f62f4b7f46a9ed7837704dbed20b30fcc45
Morgan Aubert <morgan.aubert@zoho.com>
2018-12-29 23:24:30 -0500

    Added basic snippet model

---
+++ db/migrate/20181230042028_create_snippets.rb
@@ -0,0 +1,12 @@
+class CreateSnippets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :snippets, id: :uuid do |t|
+      t.text :content, null: false
+      t.string :lexer, null: false
+      t.datetime :expire_in
+      t.boolean :is_one_time, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
