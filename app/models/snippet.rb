b500003214fb6f1e632ffd9aad5ad90cac7392c4
Morgan Aubert <morgan.aubert@zoho.com>
2018-12-30 23:00:45 -0500

    Added model annotation

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: snippets
+#
+#  id          :uuid             not null, primary key
+#  content     :text             not null
+#  lexer       :string           not null
+#  expire_in   :datetime
+#  is_one_time :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Snippet < ApplicationRecord
 end
