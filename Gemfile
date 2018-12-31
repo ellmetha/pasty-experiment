ca611264ba6743424721492c86527ddb528b0599
Morgan Aubert <morgan.aubert@zoho.com>
2018-12-31 16:24:51 -0500

    Added very basic locale configuration

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "pg", ">= 0.18"
 gem "pgcli-rails"
 gem 'puma', '~> 3.11'
 gem 'rails', '~> 5.2.2'
+gem 'rails-i18n', '~> 5.1'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module PastyExperiment
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Configure locale-related settings.
+    config.i18n.available_locales = %i[en fr]
+    config.i18n.default_locale = :en
   end
 end
