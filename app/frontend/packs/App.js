662c4e91822aab21a2854edf035024428fbf34c8
Morgan Aubert <morgan.aubert@zoho.com>
2019-01-02 17:52:28 -0500

    Added image handling in the main JS entry point and updated application layout

--- app/frontend/packs/App.js
+++ app/frontend/packs/App.js
@@ -1,3 +1,8 @@
 import '@babel/polyfill';
 
+// Imports global app stylesheet.
 import 'styles/App.scss';
+
+// Ensures all images are imported.
+const importAll = (r) => r.keys().map(r)
+importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
