2f3e694dcea1ac47f54f37fdebb886e39181e62b
Morgan Aubert <morgan.aubert@zoho.com>
2019-01-02 21:49:56 -0500

    Added eslint checks

--- app/frontend/packs/App.js
+++ app/frontend/packs/App.js
@@ -1,8 +1,9 @@
 import '@babel/polyfill';
 
 // Imports global app stylesheet.
+// eslint-disable-next-line import/no-unresolved
 import 'styles/App.scss';
 
 // Ensures all images are imported.
-const importAll = (r) => r.keys().map(r)
+const importAll = r => r.keys().map(r);
 importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
