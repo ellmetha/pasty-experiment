b8a7b440ff31c4529942920b58f34718b05b9238
Morgan Aubert <morgan.aubert@zoho.com>
2019-01-03 22:13:27 -0500

    Added error-related logic to the app form builder

--- app/helpers/app_form_builder.rb
+++ app/helpers/app_form_builder.rb
@@ -1,14 +1,14 @@
 # Extends the FormBuilder in order to allow form helpers to smoothly behave with the frontend
 # framework used to style forms and inputs.
 class AppFormBuilder < ActionView::Helpers::FormBuilder
-  %w(text_field email_field password_field).each do |selector|
-    define_method selector do |method, option = {}|
-      super(method, insert_class('input', options))
+  %w[text_field email_field password_field].each do |selector|
+    define_method selector do |method, options = {}|
+      super(method, insert_class("input #{'is-danger' if errors? method}", options))
     end
   end
 
   def text_area(method, options = {})
-    super(method, insert_class('textarea', options))
+    super(method, insert_class("textarea #{'is-danger' if errors? method}", options))
   end
 
   def errors(method)
@@ -23,15 +23,21 @@ class AppFormBuilder < ActionView::Helpers::FormBuilder
     super(method, insert_class('button is-link', options))
   end
 
-  def help_block(message, class_name: '')
-    %Q(<p class="help #{class_name}">#{message}</p>).html_safe
+  def help_block(message, class_name = '')
+    %(<p class="help #{class_name}">#{message}</p>).html_safe
   end
 
   private
 
+  # Returns true if at least one error is found for the considered attribute.
+  def errors?(attr_name)
+    object.errors[attr_name].any?
+  end
+
+  # Inserts a class name into the passed options hash.
   def insert_class(class_name, options)
     output = options.dup
-    output[:class] = ((output[:class] || "") + " #{class_name}").strip
+    output[:class] = ((output[:class] || '') + " #{class_name}").strip
     output
   end
 end
