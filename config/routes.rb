111c8ab2e82c42f0dcabf8fe5b45dc8d48440a98
Morgan Aubert <morgan.aubert@zoho.com>
2019-01-05 18:41:34 -0500

    Added devise module to project for basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :snippets, only: %i[show create], path: ''
   root 'snippets#new'
 end
---
+++ test/factories/users.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at             :datetime         not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  id                     :bigint(8)        not null, primary key
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+
+FactoryBot.define do
+  factory :user do
+  end
+end
