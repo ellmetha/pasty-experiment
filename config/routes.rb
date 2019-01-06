d4cd55398a67e1fdf18553fc1accfcd5f6a85e3d
Morgan Aubert <morgan.aubert@zoho.com>
2019-01-05 20:43:31 -0500

    Updated routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: 'users',
+             path_names: {
+               sign_in: 'signin',
+               sign_out: 'signout',
+               sign_up: 'signup'
+             }
   resources :snippets, only: %i[show create], path: ''
   root 'snippets#new'
 end
