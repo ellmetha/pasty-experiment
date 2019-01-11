5462ea21be0962ab57f12f1408c9cdf9a5cabf8e
Morgan Aubert <morgan@aubert.email>
2019-01-10 23:27:39 -0500

    Add tests for snippet controller's show action

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'factory_bot_rails'
   gem 'faker'
+  gem 'rails-controller-testing'
 end
 
 group :development do
--- test/controllers/snippet_controller_test.rb
+++ test/controllers/snippet_controller_test.rb
@@ -1,4 +1,32 @@
 require 'test_helper'
 
 class SnippetControllerTest < ActionDispatch::IntegrationTest
+  test 'snippet access works as expected' do
+    snippet = FactoryBot.create(:snippet)
+    get snippet_url(snippet)
+    assert_response :success
+    assert_select 'code', snippet.content
+  end
+
+  test 'snippet view counter is incremented when the snippet is browsed' do
+    snippet = FactoryBot.create(:snippet)
+    get snippet_url(snippet)
+    snippet.reload
+    assert_equal snippet.views_counter, 1
+  end
+
+  test 'defines a copy of the showed snippet to allow the user to edit' do
+    snippet = FactoryBot.create(:snippet)
+    get snippet_url(snippet)
+    assert_equal assigns(:new_snippet_from_current).lexer, snippet.lexer
+    assert_equal assigns(:new_snippet_from_current).content, snippet.content
+  end
+
+  test 'destroys a one-time snippet that is being viewed for the second time' do
+    snippet = FactoryBot.create(:snippet, is_one_time: true, views_counter: 1)
+    get snippet_url(snippet)
+    assert_raises(ActiveRecord::RecordNotFound) do
+      Snippet.find(snippet.id)
+    end
+  end
 end
