9b23f939fd1d5c114b66ea006495b4e3276a5929
Morgan Aubert <morgan@aubert.email>
2019-01-12 23:25:31 -0500

    Add copy button to snippet page

--- app/frontend/js/controllers/snippet/ShowController.js
+++ app/frontend/js/controllers/snippet/ShowController.js
@@ -1,9 +1,14 @@
+import Clipboard from 'clipboard';
 import hljs from 'highlight.js';
 
 
 export default {
   init() {
+    // Initializes Highlight JS.
     const block = document.querySelector('pre code');
     hljs.highlightBlock(block);
+
+    // Initializes Clipboard JS.
+    new Clipboard('#copy_snippet_url_link');
   },
 };
--- app/frontend/packs/App.js
+++ app/frontend/packs/App.js
@@ -1,12 +1,14 @@
 import '@babel/polyfill';
 
-// Imports global app stylesheet.
-// eslint-disable-next-line import/no-unresolved
-import 'styles/App.scss';
+import feather from 'feather-icons/dist/feather';
 
 import controllers from 'controllers';
 import DOMRouter from 'core/DOMRouter';
 import FlashMessages from 'core/flashMessages';
+
+// Imports global app stylesheet.
+// eslint-disable-next-line import/no-unresolved
+import 'styles/App.scss';
 
 // Ensures all images are imported.
 const importAll = r => r.keys().map(r);
@@ -16,6 +18,9 @@ importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
 const router = new DOMRouter(controllers);
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Initializes feather icons.
+  feather.replace();
+
   // Initializes the DOM router. The DOM router is used to execute specific portions of JS code for
   // each specific page.
   router.init();
