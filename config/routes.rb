8c93fc6a2e621d65d40cfde593c6f8973ce7307a
Morgan Aubert <morgan@aubert.email>
2019-01-14 22:22:33 -0500

    Added basic navbar menu for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
                sign_in: 'signin',
                sign_out: 'signout',
                sign_up: 'signup'
-             }
+             },
+             sign_out_via: 'get'
   resources :snippets, only: %i[show create], path: ''
   root 'snippets#new'
 end
