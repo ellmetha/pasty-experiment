1a2e0a394d23170a72577ed0b219741b8ebb273a
Morgan Aubert <morgan@aubert.email>
2019-01-20 12:54:57 -0500

    Add missing unit test for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#remember_me ensures that the user is remembered by default' do
+    user = FactoryBot.build(:user)
+    assert_equal '1', user.remember_me
+  end
 end
