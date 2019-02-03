c04321b1f8f18eace678b052ad6ab1f827e821ca
Morgan Aubert <morgan@aubert.email>
2019-02-02 23:50:54 -0500

    Updated Snippet GraphQL type

--- app/graphql/types/snippet_type.rb
+++ app/graphql/types/snippet_type.rb
@@ -5,12 +5,12 @@ module Types
     graphql_name 'Snippet'
     description 'The Snippet type'
 
-    field :id, ID, null: false
-    field :lexer, String, null: false
-    field :content, String, null: false
-    field :expire_in, GraphQL::Types::ISO8601DateTime, null: true
-    field :is_one_time, Boolean, null: false
-    field :views_counter, Int, null: false
+    field :id, ID, null: false, description: 'Snippet ID'
+    field :lexer, String, null: false, description: 'Snippet language'
+    field :content, String, null: false, description: 'Snippet content'
+    field :expire_in, GraphQL::Types::ISO8601DateTime, null: true, description: 'Expiration date'
+    field :is_one_time, Boolean, null: false, description: 'One-time status'
+    field :views_counter, Int, null: false, description: 'Number of views'
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: true
   end
